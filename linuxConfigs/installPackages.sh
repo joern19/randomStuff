00b773a8846d2bb795499327165715d041c3837c
jörn <joern@keepout.de>
2021-03-28 13:34:21 +0200

    Rewrote script to install all my stuff.

--- linuxConfigs/installPackages.sh
+++ linuxConfigs/installPackages.sh
@@ -1,31 +1,49 @@
-if ! [ "$EUID" -ne 0 ]
-  then echo "Please do not run as root"
-  exit
+#!/bin/bash
+
+# Syncronize everything
+# Expecting an ready to use system. Only installing and configuring tools
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Using $USER as an already configured user. Skipping user Setup..."
+else
+  echo "Root detected. Please setup a user to configure the applications for."
+  echo "exiting"
+  exit 1
 fi
 
+function printTitle() {
+  echo ""
+  echo ""
+  echo $(tput bold)$1$(tput sgr0)
+}
+
+
+printTitle "Updateing system"
 sudo pacman -Syu
-sudo pacman -S i3 playerctl feh dmenu ttf-font-awesome spotifyd rxvt-unicode xsel xfce4-terminal xcompmgr
-sudo snap install spt
 
-mkdir -p ~/.config/autostart
-echo "
-[Desktop Entry]
-Encoding=UTF-8
-Type=Application
-Name=xcompmgr
-Exec=xcompmgr
-StartupNotify=true" > ~/.config/autostart/xcompmgr.desktop
+printTitle "Installing packages"
 
-# Install font if not installed already
-FILE="~/.fonts/System\ San\ Francisco\ Display\ Bold.ttf"
-if ! [[ -f "$FILE" ]]; then
+essentail="wget git unzip python sudo"
+niceToHaveCli="tree yay ufw snapd python-pip youtube-dl"
+desktop="i3-gaps i3blocks feh dmenu ttf-font-awesome lightdm-webkit2-greeter playerctl xclip"
+guiApps="gparted firefox kdeconnect ksystemlog ksysguard pavucontrol rxvt-unicode xfce4-terminal"
+sudo pacman -S --noconfirm --needed $essentail $niceToHaveCli $desktop $guiApps
+
+#echo "idk"
+#sudo pacman -S spotifyd xsel
+
+printTitle "Installing snap apps"
+sudo systemctl enable --now snapd 
+sudo snap install code postman 
+
+printTitle "Enabeling firewall"
+sudo ufw enable
+
+if [[ -f "~/.fonts/System\ San\ Francisco\ Display\ Bold.ttf" ]]; then # The font actually consists of multiple files. But checking only one should be enogh to check if it is already installed
+  printTitle "Installing San Francisco Font"
   wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip -O tmpFile.zip
   unzip tmpFile.zip
   rm tmpFile.zip
   mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/
   rm -r YosemiteSanFranciscoFont-master/
 fi
-
-
-mkdir -p ~/.urxvt/ext
-wget https://raw.githubusercontent.com/muennich/urxvt-perls/master/keyboard-select -O ~/.urxvt/ext/keyboard-select
