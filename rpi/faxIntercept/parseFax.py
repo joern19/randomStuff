8f5f12ee82d81888fa1d99582e188e3d0365186b
Jörn Hirschfeld <joern@hirschfeld.tech>
2025-07-28 23:16:26 +0200

    Support umlaute

--- rpi/faxIntercept/parseFax.py
+++ rpi/faxIntercept/parseFax.py
@@ -8,7 +8,7 @@ import pytesseract
 from PIL import Image
 
 img = Image.open(tifFile)
-text = pytesseract.image_to_string(img)
+text = pytesseract.image_to_string(img, lang='deu')
 
 with open("/webhookUrl.txt") as webhookUrlFile:
     webhookUrl = webhookUrlFile.readlines()[0].strip()
