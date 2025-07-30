14d00d0e13128887d537880d9a4b3531afb952ed
Jörn Hirschfeld <joern@hirschfeld.tech>
2025-07-30 23:42:23 +0200

    Fix onfax lua call

--- rpi/faxIntercept/onfax.lua
+++ rpi/faxIntercept/onfax.lua
@@ -1,4 +1,4 @@
-TifFile = argv[1]
+TifFile = session:getVariable("fax_file")
 
 PythonCommand = "python3 /usr/local/freeswitch/conf/parseFax.py " .. TifFile
 freeswitch.consoleLog("info", "onfax. running: '" .. PythonCommand .. "'\n");
