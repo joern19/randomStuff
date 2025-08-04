8c166fddd2d09be2885761db31f32fd261c69db7
Jörn Hirschfeld <joern@hirschfeld.tech>
2025-08-05 00:27:06 +0200

    Send an mqtt message onfax

--- rpi/faxIntercept/onfax.lua
+++ rpi/faxIntercept/onfax.lua
@@ -3,4 +3,4 @@ TifFile = session:getVariable("fax_file")
 PythonCommand = "python3 /usr/local/freeswitch/conf/parseFax.py " .. TifFile
 freeswitch.consoleLog("info", "onfax. running: '" .. PythonCommand .. "'\n");
 os.execute(PythonCommand .. " > /tmp/parseFax.log &")
-
+pcall(function () os.execute("kill -USR1 2") end)
--- rpi/faxIntercept/run_freeswitch.sh
+++ rpi/faxIntercept/run_freeswitch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-podman run -it --network host --restart=always -v $(dirname "$0"):/usr/local/freeswitch/scripts -v $(dirname "$0"):/usr/local/freeswitch/conf -v /webhookUrl.txt:/webhookUrl.txt freeswitch-custom "/usr/local/freeswitch/bin/freeswitch"
+podman run -it --network host --restart=always -v $(dirname "$0"):/usr/local/freeswitch/scripts -v $(dirname "$0"):/usr/local/freeswitch/conf -v /webhookUrl.txt:/webhookUrl.txt --env-file=/home/joern/alarm.envfile freeswitch-custom
