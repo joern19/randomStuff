d3118d8945ab6a8cccd9d3bffbb855375dc4a79e
Jörn Hirschfeld <joern@hirschfeld.tech>
2026-05-05 23:27:04 +0200

    restart unless stopped

---
+++ rpi/simpleservices/.generic.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+PARENT_COMMAND=$(ps -o comm= $PPID)
+ssh pi@kaboom.l.joern19.de "podman run -d --restart=unless-stopped --name ${PARENT_COMMAND%.*} --replace $@"
+
--- rpi/upload-build-deloy-generic.sh
+++ rpi/upload-build-deloy-generic.sh
@@ -4,5 +4,5 @@ set -e
 
 name=$(basename $(pwd))
 scp -r . pi@kaboom.l.joern19.de:/tmp/$name
-ssh pi@kaboom.l.joern19.de "cd /tmp/$name && podman build -t randomstuff_$name . && podman run -d --name $name --replace $@ randomstuff_$name"
+ssh pi@kaboom.l.joern19.de "cd /tmp/$name && podman build -t randomstuff_$name . && podman run -d --restart=unless-stopped --name $name --replace $@ randomstuff_$name"
 
